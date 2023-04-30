bfe1777cc471885aa974b65d1552a3b96bae38d3
yonathanjulianputra.irawan <yonathanjulianputra.irawan@student.kuleuven.be>
2023-04-30 16:47:49 +0200

    Test latency in variable get length

--- rmi_server/src/hotel/BookingInterface.java
+++ rmi_server/src/hotel/BookingInterface.java
@@ -2,11 +2,14 @@ package hotel;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface BookingInterface extends Remote {
      int connect() throws RemoteException;
+     List<Room> getRooms() throws RemoteException;
      String printRooms() throws RemoteException;
      String currentCart(Integer key) throws RemoteException;
      AddBookingStatus addBookingDetail(Integer key, BookingDetail bookingDetail) throws RemoteException;
-     boolean bookAll(Integer key) throws RemoteException;;
+     boolean bookAll(Integer key) throws RemoteException;
+     void setNumberOfRooms(int noOfRooms) throws RemoteException;
 }
